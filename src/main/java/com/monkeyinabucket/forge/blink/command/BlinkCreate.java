c426633c2afca5fb2980f7422f089051484abf61
Donald Tyler <domhnall.tilliere@gmail.com>
2018-01-27 08:45:53 -0600

    Add command to create runes
    For debugging purposes.

---
+++ src/main/java/com/monkeyinabucket/forge/blink/command/BlinkCreate.java
@@ -0,0 +1,89 @@
+package com.monkeyinabucket.forge.blink.command;
+
+import com.monkeyinabucket.forge.blink.Blink;
+import net.minecraft.block.Block;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+/**
+ * Handler for the /blink-create command.
+ */
+public class BlinkCreate extends BaseCommand implements ICommand {
+
+  /**
+   * The primary name that can be used to execute this command.
+   */
+  public static final String NAME = "blink-create";
+
+  /**
+   * Constructor.
+   */
+  public BlinkCreate() {
+    super();
+
+    this.aliases.add(BlinkCreate.NAME);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getCommandName() {
+    return BlinkCreate.NAME;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getCommandUsage(ICommandSender sender) {
+    return BlinkCreate.NAME;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void processCommand(ICommandSender sender, String[] command) {
+    if (!(sender instanceof EntityPlayerMP)) {
+      return;
+    }
+
+    EntityPlayerMP player = (EntityPlayerMP) sender;
+
+    int distance = 64;
+    MovingObjectPosition targetPos = player.rayTrace(distance, 1.0F);
+
+    World world = player.getEntityWorld();
+
+    int center = Blink.halfRuneSize;
+    int cornerX = targetPos.blockX - Blink.halfRuneSize;
+    int cornerZ = targetPos.blockZ - Blink.halfRuneSize;
+    int y = targetPos.blockY;
+
+    for (int col = 0; col < Blink.runeSize; ++col) {
+      for (int row = 0; row < Blink.runeSize; ++row) {
+        int x = cornerX + col;
+        int z = cornerZ + row;
+
+        Block block;
+        if ((col == center && (row == center - 1 || row == center + 1)) || row == center && (col == center - 1 || col == center + 1)) {
+          // blocks immediately north, south, east & west of the center are signature.
+          block = Blocks.stone;
+        } else if (col == center && row == center) {
+          // center block is the Rune Core
+          block = Blink.runecore;
+        } else {
+          // the shell is obsidian
+          block = Blocks.obsidian;
+        }
+
+        world.setBlock(x, y, z, block);
+      }
+    }
+  }
+}
